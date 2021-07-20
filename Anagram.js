6b59bbe9b02b4ca3c3b005160627cb43c06baa1f
HusseinGuevara <80933892+HusseinGuevara@users.noreply.github.com>
2021-07-20 09:59:05 -0700

    Add files via upload

---
+++ Anagram.js
@@ -0,0 +1,45 @@
+/* 
+An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+Is there a quick way to determine if they aren't an anagram before spending more time?
+Given two strings
+return whether or not they are anagrams
+*/
+
+const strA1 = "yes";
+const strB1 = "eys";
+const expected1 = true;
+
+const strA2 = "yes";
+const strB2 = "eYs";
+const expected2 = true;
+
+const strA3 = "no";
+const strB3 = "noo";
+const expected3 = false;
+
+const strA4 = "silent";
+const strB4 = "listen";
+const expected4 = true;
+
+function isAnagram(s1, s2) {
+    if(s1.length !== s2.length) {
+        return false;
+    }
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+    for(var char of s1) {
+        // if char is not included in s2, return false
+        if(!s2.includes(char)) {
+            return false
+        } else {
+            return true;
+        }
+    }
+    
+
+}
+console.log(isAnagram(strA1, strB1));
+console.log(isAnagram(strA2, strB2));
+console.log(isAnagram(strA3, strB3));
+console.log(isAnagram(strA4, strB4));
